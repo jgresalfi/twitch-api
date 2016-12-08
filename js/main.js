95eedcfc87b52f92426d868a65d5b96cc745c426
Jason Gresalfi <jason.gresalfi@gmail.com>
2016-12-08 10:09:52 -0500

    Added favicon

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 	//AJAX call to Twitch API
-	var twitchUrl = 'https://wind-bow.gomix.me/twitch-api/streams/ESL_SC2';
+	var twitchUrl = 'http://wind-bow.gomix.me/twitch-api/streams/ESL_SC2';
 
 	function resRender(data) {
 		console.log(data)
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	function getResponse(url, callback) {
 		var xhr = new XMLHttpRequest();
-		xhr.withCredentials = false;
-		xhr.open('GET', url, true);
+		xhr.open('GET', url);
+		// xhr.withCredentials = true;
 		xhr.onload = function() {
 			if (this.status >= 200 && this.status < 400) {
 				console.log(this.status);
