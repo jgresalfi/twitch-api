d1d3f4635498d233e28a198cc9094fd1e66a34c4
Jason Gresalfi <jason.gresalfi@gmail.com>
2016-12-08 22:11:03 -0500

    Many changes: created HTML template for MDL cards, started building AJAX, render and helper functions, CSS cleanup

--- js/main.js
+++ js/main.js
@@ -3,13 +3,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 	//AJAX call to Twitch API
-	var twitchUrl = 'http://wind-bow.gomix.me/twitch-api/streams/ESL_SC2';
+	var streamsUrl = 'https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api/streams/',
+	    channelUrl = 'https://cors-anywhere.herokuapp.com/https://wind-bow.gomix.me/twitch-api/channels/',
+	    ajaxUrl = '',
+			twitchUsers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
 
-	function resRender(data) {
+	function resRender(data, arr) {
 		console.log(data)
+		//This function will handle rendering data/elements to page
 	}
 
-		function getResponse(url, callback) {
+	//This is the AJAX request
+	function getResponse(url, callback) {
 		var xhr = new XMLHttpRequest();
 		xhr.open('GET', url);
 		xhr.onload = function() {
@@ -18,9 +23,30 @@ document.addEventListener('DOMContentLoaded', function() {
 				callback(this.response);
 			} else { console.log(this.response); }
 		}
-		xhr.send();
-	} //End getResponse
+	xhr.send();
+	}
 
-	getResponse(twitchUrl, resRender);
+	//This function iterates over user array, compiles URLs
+	function userLoop(urlType, arr) {
+		arr.forEach(function(e) {
+			ajaxUrl = urlType + e;
+			console.log(ajaxUrl)
+		});
+	}
 
+	function statusCheck(data) {
+		if (data.stream === null) {
+			userLoop(channelUrl, twitchUsers, resRender);
+		}
+		else {
+
+		}
+	}
+
+	// getResponse(twitchUrl, resRender);
+	userLoop(streamsUrl, twitchUsers);
 });
+
+
+
+
