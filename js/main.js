449bb3f08bb09a595cc5124f6ffccd77dd327b87
Jason Gresalfi <jason.gresalfi@gmail.com>
2016-12-13 22:10:36 -0600

    Added user logos and logic to handle user not found/deleted

--- js/main.js
+++ js/main.js
@@ -10,23 +10,27 @@ document.addEventListener('DOMContentLoaded', function() {
         twitchUsers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
 
     function resRender(data, arr) {
-
-        //HTML card template for rendering data to page - insert logic to choose stream or channel template...
-
-        if (data.stream) {
+        //Stream and channel card data to page via HTML templates
+        if (data.status === 404) {
+            var noUserTmpl = document.getElementById('channel-template').content.cloneNode(true);
+            noUserTmpl.getElementById('chan-status').innerHTML = '<h3>User Not Found</h3>';
+            document.body.appendChild(noUserTmpl);
+        } else if (data.stream) {
             var streamTmpl = document.getElementById('stream-template').content.cloneNode(true),
                 socialShare = streamTmpl.getElementById('share-button');
 
             streamTmpl.getElementById('screen-name').innerHTML = data.stream.channel.display_name;
             streamTmpl.getElementById('chan-status').innerHTML = data.stream.channel.status;
             streamTmpl.getElementById('stream-link').setAttribute('href', data.stream.channel.url);
+            streamTmpl.querySelector('img').setAttribute('src', data.stream.channel.logo);
             document.body.appendChild(streamTmpl);
-        } else {
+        } else if (!data.stream) {
             var chanTmpl = document.getElementById('channel-template').content.cloneNode(true),
                 socialShare = chanTmpl.getElementById('share-button');
 
             chanTmpl.getElementById('screen-name').innerHTML = data.display_name;
             chanTmpl.getElementById('chan-status').innerHTML = '<h3>Offline</h3>';
+            chanTmpl.querySelector('img').setAttribute('src', data.logo);
             document.body.appendChild(chanTmpl);
         }
     }
@@ -38,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function() {
         xhr.onload = function() {
             if (this.status >= 200 && this.status < 400) {
                 callback(JSON.parse(this.response));
-            } else { console.log(this.response); }
+            } else { console.log('AJAX call error: ' + this.response); }
         }
         xhr.send();
     }
